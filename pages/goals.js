62b948d9c1b4dc89715e791bfdd9bdc5749c27ea
Holland Pleskac <hollandpleskac@gmail.com>
2021-08-10 16:03:04 -0700

    goals

---
+++ pages/goals.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navigation from '../components/nav'
+import AuthContext from '../context/authContext'
+
+const Goals = () => {
+  return (
+    <Navigation>
+      Goals Screen
+    </Navigation>
+  )
+}
+
+export default Goals
